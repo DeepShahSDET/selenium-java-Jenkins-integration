d5d88a9bd1f1fbbfcd2be89695ca05eae255e9f7
DeepShahSDET <deep@atharvasystem.com>
2024-06-24 17:33:43 +0530

    selenium-java-Jenkins-integration

---
+++ src/test/java/jenkinsdemo/jenkins.java
@@ -0,0 +1,21 @@
+package jenkinsdemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class jenkins {
+
+	@Test
+	public void test1() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.saucedemo.com/v1/");
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("login-button")).click();
+		driver.quit();
+	}
+
+}

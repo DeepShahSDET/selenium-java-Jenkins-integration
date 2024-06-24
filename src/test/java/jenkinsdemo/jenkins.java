f8afa0c21badef55152b1901e509b299db448894
DeepShahSDET <deep@atharvasystem.com>
2024-06-24 17:58:30 +0530

    Parameted added

--- src/test/java/jenkinsdemo/jenkins.java
+++ src/test/java/jenkinsdemo/jenkins.java
@@ -3,13 +3,23 @@ package jenkinsdemo;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class jenkins {
 
+	@Parameters("Browser")
 	@Test
-	public void test1() {
-		WebDriver driver = new ChromeDriver();
+	public void test1(String browser) {
+		WebDriver driver = null;
+		System.out.println("Browser: " + browser);
+
+		if (browser.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		} else {
+			driver = new EdgeDriver();
+		}
 		driver.manage().window().maximize();
 		driver.get("https://www.saucedemo.com/v1/");
 		driver.findElement(By.id("user-name")).sendKeys("standard_user");
